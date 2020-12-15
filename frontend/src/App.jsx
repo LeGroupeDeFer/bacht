56903a15c601065d909e07dc0af4fa0a03dc1865
latour-gui <guillaume.latour@student.unamur.be>
2020-12-15 15:49:25 +0100

    adding wip for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// App :: None => Component
+function App(_) {
+    return (
+        <h1>Hello noob !</h1>
+    );
+}
+export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Bacht [idasm451]
+
+This is a readme file with relevant informations concerning this repository.
