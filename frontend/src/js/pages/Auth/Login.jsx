394cf717221c3de84dc53541f4d625d0f40e7232
findot <findot@protonmail.com>
2020-12-23 19:21:03 +0100

    [Frontend] Sidebar

--- frontend/src/js/pages/Auth/Login.jsx
+++ frontend/src/js/pages/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 
 import { connectAuth } from 'sharea/store/auth';
