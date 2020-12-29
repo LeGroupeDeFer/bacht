f7b0c70ecc8d119c37d2507865e94b68ca03495a
latour-gui <guillaume.latour@student.unamur.be>
2020-12-29 23:35:28 +0100

    [backend] GET media + improve other C(R)UD features

--- backend/src/main/scala/be/unamur/infom451/bacht/controllers/api/MediaController.scala
+++ backend/src/main/scala/be/unamur/infom451/bacht/controllers/api/MediaController.scala
@@ -1,12 +1,9 @@
 package be.unamur.infom451.bacht.controllers.api
 
-import com.twitter.util.{Future => TwitterFuture}
 import wvlet.airframe.http.{Endpoint, HttpMethod, Router}
 import be.unamur.infom451.bacht.lib._
-import be.unamur.infom451.bacht.models.Media
 import be.unamur.infom451.bacht.models.MediaTable.Media
 
-import java.sql.Blob
 import scala.concurrent.Future
 
 object MediaController extends Guide {
@@ -20,12 +17,12 @@ object MediaController extends Guide {
   case class MediaCreationRequest(
     name: String,
     content: String,
-    kind : String,
-    shareaId : Int
+    kind: String,
+    shareaId: Int
   )
 
   object DetailedMediaInfo {
-    def from(media: Media) : DetailedMediaInfo =
+    def from(media: Media): DetailedMediaInfo =
       DetailedMediaInfo(
         media.id.get,
         media.name,
@@ -35,53 +32,65 @@ object MediaController extends Guide {
         media.shareaId
       )
   }
+
   case class DetailedMediaInfo(
-    id : Int,
-    name : String,
-    kind : String,
-    content : String,
-    author : Int,
-    shareaId : Int
+    id: Int,
+    name: String,
+    kind: String,
+    content: String,
+    author: Int,
+    shareaId: Int
   )
+
   type DetailedMediaResponse = DetailedMediaInfo
 
-  type MediaUpdateRequest = MediaCreationRequest
+  case class MediaUpdateRequest(
+    name: String,
+    content: String,
+  )
+
 }
 
 @Endpoint(path = "/api/media")
 trait MediaController {
 
   import MediaController._
-  //import ShareaController._
+
+  @Endpoint(method = HttpMethod.GET, path = "/kinds")
+  def availableMediaKinds: Seq[String] = Seq("text", "sound", "video", "image")
 
   @Endpoint(method = HttpMethod.POST, path = "/")
-  def insertMedia (request: MediaCreationRequest): Future[DetailedMediaResponse] =
+  def insertMedia(request: MediaCreationRequest): Future[DetailedMediaResponse] =
     withUser(u => u)
-      .map(user =>
-        Media(None, request.name, request.kind,request.content.getBytes,user.id.get,request.shareaId)
+      .map(user => {
+        if (!availableMediaKinds.contains(request.kind)) {
+          throw invalidAttribute
+        }
+        Media(None, request.name, request.kind, request.content.getBytes, user.id.get, request.shareaId)
+      }
       )
       .flatMap(_.insert)
       .map(DetailedMediaInfo.from)
       .recoverWith(ErrorResponse.recover(409))
 
+  @Endpoint(method = HttpMethod.GET, path = "/:id")
+  def getMedia(id: Int): Future[DetailedMediaResponse] =
+    Media.byId(id)
+      .map(optionMedia => optionMedia.map(DetailedMediaInfo.from).get)
+      .recoverWith(ErrorResponse.recover(404))
+
   @Endpoint(method = HttpMethod.DELETE, path = "/:id")
-  def deleteMedia (id : Int): Future[Unit] =
+  def deleteMedia(id: Int): Future[Boolean] =
     Media.byId(id)
       .flatMap(optionMedia => optionMedia.get.delete)
       .recoverWith(ErrorResponse.recover(404))
 
-  @Endpoint(method = HttpMethod.PUT, path= "/:id")
-  def updateMedia(id: Int, change : MediaUpdateRequest) : Future[Media] =
+  @Endpoint(method = HttpMethod.PUT, path = "/:id")
+  def updateMedia(id: Int, change: MediaUpdateRequest): Future[Media] =
     Media.byId(id)
       .map(media => media.get.copy(
-        name = change.name, content = change.content.getBytes, kind = change.kind, shareaId = change.shareaId)
+        name = change.name, content = change.content.getBytes)
       )
       .flatMap(mediaCopy => mediaCopy.update)
       .recoverWith(ErrorResponse.recover(404))
-
 }
-
-
-
-
-
