LeGroupeDeFer/bacht

commit 250042d0d8f35fd39d7b5a74a26728683cab28ba

[frontend] add structure for profile + extract component TabNav + uniform css for profile & dashboard

frontend/src/js/pages/Auth/index.js
@@ -5,9 +5,9 @@ import { connectAuth } from 'sharea/store/auth';
 import Loader from 'sharea/component/Loader';
 import Heading from 'sharea/component/Heading';
 
-import Login from './Login';
-import Logout from './Logout';
-import Register from './Register';
+import Login from 'sharea/Login';
+import Logout from 'sharea/Logout';
+import Register from 'sharea/Register';
 
 
 // Auth :: None => Component

frontend/src/js/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App';
-import store from './store';
+import App from 'sharea/App';
+import store from 'sharea/store';
 
 
 const root = document.getElementById('root');

frontend/src/js/pages/index.js
@@ -1,1 +1,1 @@
-export * from './Home';
+export * from 'sharea/Home';

frontend/src/js/App.jsx
@@ -5,14 +5,14 @@ import {
   useHistory
 } from 'react-router-dom';
 
-import { connectAuth } from './store/auth';
-import Loader from './component/Loader';
+import { connectAuth } from 'sharea/store/auth';
+import Loader from 'sharea/component/Loader';
 
 
-const Home = lazy(() => import('./pages/Home'));
-const Auth = lazy(() => import('./pages/Auth'));
-const Profile = lazy(() => import('./pages/Profile'));
-const Discover = lazy(() => import('./pages/Discover'));
+const Home = lazy(() => import('sharea/pages/Home'));
+const Auth = lazy(() => import('sharea/pages/Auth'));
+const Profile = lazy(() => import('sharea/pages/Profile'));
+const Discover = lazy(() => import('sharea/pages/Discover'));
 
 
 // App :: None => Component

frontend/src/js/component/layout/TabNav.jsx
@@ -1,0 +1,17 @@
+import {Nav} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+function TabNav(props) {
+  return (
+    <Nav defaultActiveKey={props.default}>
+      {props.links.map(({ uri, title }) => (
+        <Nav.Item key={uri}>
+          <Nav.Link as={NavLink} exact to={uri}>{title}</Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+  );
+}
+
+export default TabNav;

frontend/src/js/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Nav, Container, Row, Col } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
 
 import Sidebar from 'sharea/component/layout/Sidebar';
 import { connectAuth } from 'sharea/store/auth';
-import { capitalize } from '../lib';
+import { capitalize } from 'sharea/lib';
+import TabNav from "sharea/component/layout/TabNav";
 
 
 const links = [
@@ -22,22 +21,6 @@ const links = [
   },
 ];
 
-
-function DashboardNav(_) {
-
-  return (
-    <Nav defaultActiveKey="/dashboard">
-      {links.map(({ uri, title }) => (
-        <Nav.Item key={uri}>
-          <Nav.Link as={NavLink} exact to={uri}>{title}</Nav.Link>
-        </Nav.Item>
-      ))}
-    </Nav>
-  );
-
-}
-
-
 // Home :: None => Component
 function Home({ token }) {
 
@@ -48,7 +31,7 @@ function Home({ token }) {
         <div className="inner-content">
           <div className="heading">
             <h1>Hi, {capitalize(token.sub)}</h1>
-            <DashboardNav />
+            <TabNav links={links} default="/dashboard" />
           </div>
         </div>
       </main>

frontend/src/js/pages/Profile.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 import Sidebar from 'sharea/component/layout/Sidebar';
-import { connectAuth } from 'sharea/store/auth';
+import {connectAuth} from 'sharea/store/auth';
+import TabNav from "sharea/component/layout/TabNav";
+
+const links = [
+  {
+    uri: '/profile',
+    title: 'Info',
+  },
+  {
+    uri: '/profile/shareas',
+    title: 'Shareas',
+  },
+  {
+    uri: '/profile/following',
+    title: 'Following',
+  },
+  {
+    uri: '/profile/like',
+    title: 'Like'
+  }
+];
 
 
-const UserMedias = connectAuth(({ token }) => {
+const UserMedias = connectAuth(({token}) => {
 
   // TODO - User medias List
   const username = token.sub;
@@ -12,23 +32,33 @@ const UserMedias = connectAuth(({ token }) => {
   return (
     <div className="medias">
       Hello, {username}!
-      { /* Met les medias ici pour le moment Alex! */ }
+      { /* Met les medias ici pour le moment Alex! */}
     </div>
   );
 
 });
 
+const ProfileName = connectAuth((props, {token}) =>  {
+  const profileName = (props.username === undefined || props.username === token.sub)
+    ? 'My'
+    : `${props.username}'s`;
 
-function Profile(_) {
+  return <h1>{profileName} profile</h1>
+});
 
+
+function Profile({ token }) {
   return (
     <div className="section profile">
-      <Sidebar />
+      <Sidebar/>
       <main className="content">
-        <div className="heading">
-          <h1>Profile</h1>
+        <div className="inner-content">
+          <div className="heading">
+            <ProfileName name={token.sub}/>
+            <TabNav links={links} default="/profile" />
+          </div>
         </div>
-        <UserMedias />
+        {/*<UserMedias/>*/}
       </main>
     </div>
   );
@@ -36,4 +66,4 @@ function Profile(_) {
 }
 
 
-export default Profile;
+export default connectAuth(Profile);

frontend/src/scss/layout/_layout.scss
@@ -21,12 +21,6 @@
   display: flex;
   height: 100%;
 
-}
-
-/* ------------------------------- DASHBOARD ------------------------------- */
-
-.dashboard {
-
   .heading {
 
     background: white;
@@ -57,6 +51,13 @@
     }
 
   }
+}
+
+/* ------------------------------- DASHBOARD ------------------------------- */
+
+.dashboard {
+
+
 
 }
 
@@ -133,12 +134,6 @@ main {
 .profile {
 
   // TODO - /profile scss
-
-  .heading {
-
-    padding: 1em;
-
-  }
 
 }
 

commit 6f5a64ebbd1dacbdcb82314b5e1779e517150eda

[frontend] allow correct binding between 'sharea' and src/js + fix bug introduced in 250042d

frontend/src/js/pages/Auth/index.js
@@ -5,9 +5,9 @@ import { connectAuth } from 'sharea/store/auth';
 import Loader from 'sharea/component/Loader';
 import Heading from 'sharea/component/Heading';
 
-import Login from 'sharea/Login';
-import Logout from 'sharea/Logout';
-import Register from 'sharea/Register';
+import Login from 'sharea/pages/Auth/Login';
+import Logout from 'sharea/pages/Auth/Logout';
+import Register from 'sharea/pages/Auth/Register';
 
 
 // Auth :: None => Component