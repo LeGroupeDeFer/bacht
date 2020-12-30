1841afc546348cbbe3ca7b53089c1811c8ab8f36
Alexandre Amand <alexandre.amand@student.unamur.be>
2020-12-30 21:25:28 +0100

    [FRONTEND] ShareaCard + UsersTable

--- frontend/src/js/pages/Home.jsx
+++ frontend/src/js/pages/Home.jsx
@@ -1,42 +1,87 @@
 import React from 'react';
 
 import Sidebar from 'sharea/component/layout/Sidebar';
-import { connectAuth } from 'sharea/store/auth';
-import { capitalize } from 'sharea/lib';
+import {connectAuth} from 'sharea/store/auth';
+import {capitalize} from 'sharea/lib';
 import TabNav from "sharea/component/layout/TabNav";
+import ShareasList from "sharea/pages/Sharea";
+import UsersList from "sharea/pages/UsersListing";
 
 
 const links = [
-  {
-    uri: '/dashboard',
-    title: 'Recent',
-  },
-  {
-    uri: '/dashboard/own',
-    title: 'Your Shareas',
-  },
-  {
-    uri: '/dashboard/friends',
-    title: 'Friends Shareas',
-  },
+    {
+        uri: '/dashboard',
+        title: 'Recent',
+    },
+    {
+        uri: '/dashboard/own',
+        title: 'Your Shareas',
+    },
+    {
+        uri: '/dashboard/friends',
+        title: 'Friends Shareas',
+    },
+];
+const shareas = [
+    {
+        name: "Cat Species",
+        description: "Presentation of the different cat species that are visible in Belgium",
+        creatorId: 1001,
+        id: 9001,
+    },
+    {
+        name: "INFOM451",
+        description: "Sharea used by Prof. Jacquet to show useful resources and exchange information with students",
+        creatorId: 1002,
+        id: 9002,
+    },
+    {
+        name: "Brainstorming",
+        description: "Sharea for an enterprise that makes a brainstorming before developing a new software",
+        creatorId: 1003,
+        id: 9003,
+    },
 ];
 
-// Home :: None => Component
-function Home({ token }) {
+const users = [
+    {
+        id : 1001,
+        username : "Fouiny",
+        isLiked : "True",
+        likedBy : 12,
+    },
+    {
+        id : 1002,
+        username : "Batman",
+        isLiked : "False",
+        likedBy : 1,
+    },
+    {
+        id : 1003,
+        username : "JCVD",
+        isLiked : "True",
+        likedBy : 153,
+    }
+]
 
-  return (
-    <div className="section dashboard">
-      <Sidebar />
-      <main className="content">
-        <div className="inner-content">
-          <div className="heading">
-            <h1>Hi, {capitalize(token.sub)}</h1>
-            <TabNav links={links} default="/dashboard" />
-          </div>
+// Home :: None => Component
+function Home({token}) {
+
+    return (
+        <div className="section dashboard">
+            <Sidebar/>
+            <main className="content">
+                <div className="inner-content">
+                    <div className="heading">
+                        <h1>Hi, {capitalize(token.sub)}</h1>
+                        <TabNav links={links} default="/dashboard"/>
+                    </div>
+                    <ShareasList shareas={shareas}/>
+                    <UsersList users={users}/>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 
 }
 
---
+++ frontend/src/js/pages/Sharea.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {Card, CardDeck} from "react-bootstrap";
+import {Link} from "react-router-dom";
+
+function ShareaCard(props) {
+    return (
+        <Card>
+            <Link to={`/sharea/${props.id}`}>
+                <Card.Body>
+                    <Card.Title> {props.name}</Card.Title>
+                    <Card.Text>
+                        <div>{props.description}</div>
+                        <div>{props.id}</div>
+                    </Card.Text>
+                </Card.Body>
+            </Link>
+            <Card.Footer>
+                <small className="TODO"><Link to={`/user/${props.creatorId}`}>@{props.creatorId}</Link> Got % likes </small>
+            </Card.Footer>
+        </Card>
+    )
+}
+
+function ShareasList(props) {
+    return (
+        <CardDeck>
+            {
+                props.shareas.map(
+                    sh => (<ShareaCard
+                        key={sh.id}
+                        name={sh.name}
+                        description={sh.description}
+                        id={sh.id}
+                        creatorId={sh.creatorId}
+                    />)
+                )
+            }
+        </CardDeck>
+    )
+}
+
+export default ShareasList;
+
+
