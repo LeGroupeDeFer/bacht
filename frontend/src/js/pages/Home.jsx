a414245de1eb7477f353472c2b80a5efacbe66db
latour-gui <guillaume.latour@student.unamur.be>
2020-12-31 12:40:14 +0100

    [frontend] rework home page : own shareas + liked shareas

--- frontend/src/js/pages/Home.jsx
+++ frontend/src/js/pages/Home.jsx
@@ -2,69 +2,46 @@ import React, {useEffect} from 'react';
 
 import TabNav from 'sharea/component/layout/TabNav';
 import Sharea from 'sharea/component/Sharea';
-import { connectUser } from 'sharea/store/user';
-import { capitalize } from 'sharea/lib';
+import {connectUser} from 'sharea/store/user';
+import {capitalize} from 'sharea/lib';
+import Switch from 'react-bootstrap/Switch';
+import {Route} from 'react-router-dom';
 
 
 const links = [
   {
     uri: '/dashboard',
-    title: 'Recent',
-  },
-  {
-    uri: '/dashboard/own',
     title: 'Your Shareas',
   },
   {
-    uri: '/dashboard/friends',
-    title: 'Friends Shareas',
+    uri: '/dashboard/liked',
+    title: 'Liked Shareas',
   },
 ];
 
 const shareas = [
   {
-    name: "Cat Species",
-    description: "Presentation of the different cat species that are visible in Belgium",
+    name: 'Cat Species',
+    description: 'Presentation of the different cat species that are visible in Belgium',
     creatorId: 1001,
     id: 9001,
   },
   {
-    name: "INFOM451",
-    description: "Sharea used by Prof. Jacquet to show useful resources and exchange information with students",
+    name: 'INFOM451',
+    description: 'Sharea used by Prof. Jacquet to show useful resources and exchange information with students',
     creatorId: 1002,
     id: 9002,
   },
   {
-    name: "Brainstorming",
-    description: "Sharea for an enterprise that makes a brainstorming before developing a new software",
+    name: 'Brainstorming',
+    description: 'Sharea for an enterprise that makes a brainstorming before developing a new software',
     creatorId: 1003,
     id: 9003,
   },
 ];
 
-const users = [
-  {
-    id: 1001,
-    username: "Fouiny",
-    isLiked: "True",
-    likedBy: 12,
-  },
-  {
-    id: 1002,
-    username: "Batman",
-    isLiked: "False",
-    likedBy: 1,
-  },
-  {
-    id: 1003,
-    username: "JCVD",
-    isLiked: "True",
-    likedBy: 153,
-  }
-]
-
 // Home :: None => Component
-function Home({ currentUser, status }) {
+function Home({currentUser, status}) {
 
   if (status === 'loading')
     return <>Loading...</>;
@@ -74,9 +51,16 @@ function Home({ currentUser, status }) {
       <div className="inner-content">
         <div className="heading">
           <h1>Hi, {capitalize(currentUser.username)}</h1>
-          <TabNav links={links} default="/dashboard"/>
+          <TabNav links={links} default="/dashboard" />
         </div>
-        <Sharea.List shareas={shareas} />
+        <Switch>
+          <Route exact path="/dashboard">
+            <Sharea.List shareas={shareas} />
+          </Route>
+          <Route exact path="/dashboard/liked">
+            <Sharea.List shareas={shareas} />
+          </Route>
+        </Switch>
       </div>
     </main>
   );
